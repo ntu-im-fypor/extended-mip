8e13c5f36d82f03576ee47bb0879778f6e821008
Brian Konr <b08705038@ntu.edu.tw>
2022-10-14 00:22:20 +0800

    finish 4 constraints and objective func

--- extended/complete_mip_gurobi.py
+++ extended/complete_mip_gurobi.py
@@ -1,4 +1,5 @@
 from gurobipy import *
+import math
 
 class Parameters:
     def __init__(self) -> None:
@@ -31,7 +32,18 @@ class Parameters:
         1. need to discuss the data input format
         2. need to implement the function to read data properly
         """
-        pass
+
+        """
+        range definition for sets
+        i = 1, 2, ..., n_I
+        m = 1, 2, ..., n_i^M
+        j = 1, 2, ..., n_J
+        """
+        self.I = list(range(1, self.parameters.Number_of_Stages + 1))
+        self.M = []
+        for machine_num in self.parameters.Number_of_Machines:
+            self.M.append(list(range(1, machine_num + 1)))
+        self.J = list(range(1, self.parameters.Number_of_Jobs + 1))
 
 class CompleteMIPModel:
     def __init__(self, parameters: Parameters, run_time_limit=300, mip_gap=0.01) -> None:
@@ -39,20 +51,11 @@ class CompleteMIPModel:
         self.parameters = parameters
         self.model.setParam('TimeLimit', run_time_limit)
         self.model.setParam('MIPGap', mip_gap)
+        self.add_variables()
+        self.add_constraints()
+        self.set_objective()
     def add_variables(self) -> None:
         # define decision variables
-        
-        """
-        range definition for sets
-        i = 1, 2, ..., n_I
-        m = 1, 2, ..., n_i^M
-        j = 1, 2, ..., n_J
-        """
-        I = list(range(1, self.parameters.Number_of_Stages + 1))
-        M = []
-        for machine_num in self.parameters.Number_of_Machines:
-            M.append(list(range(1, machine_num + 1)))
-        J = list(range(1, self.parameters.Number_of_Jobs + 1))
 
         # r_imj: 1 if job j is completed on machine m in stage i or 0, otherwise
         self.r_imj = self.gp_model.addVars(I, M, J, vtype=GRB.BINARY, name="r")
@@ -65,30 +68,67 @@ class CompleteMIPModel:
         # p_imj = effective production time of job j on machine m in stage i
         self.p_imj = self.gp_model.addVars(I, M, J, vtype=GRB.CONTINUOUS, name="p")
         # x_imjk = 1 if job j precedes job k on machine m in stage i or 0, otherwise
-        x = {}
-        for i in I:
-            M_i = M[i - 1]
+        self.x = {}
+        for i in self.I:
+            M_i = self.M[i - 1]
             for m in M_i:
-                for j in J:
-                    for k in J:
+                for j in self.J:
+                    for k in self.J:
                         if j != k:
-                            x[i, m, j, k] = self.gp_model.addVar(vtype=GRB.BINARY, name=f"x_{i}_{m}_{j}_{k}")
+                            self.x[i, m, j, k] = self.gp_model.addVar(vtype=GRB.BINARY, name=f"x_{i}_{m}_{j}_{k}")
         # y_imj^Before = 1 if maintenance precedes job j on machine m in stage i or 0, otherwise
         self.y_imj_before = self.gp_model.addVars(I, M, J, vtype=GRB.BINARY, name="y_before")
         # y_imj^After = 1 if job j precedes maintenance on machine m in stage i or 0, otherwise
         self.y_imj_after = self.gp_model.addVars(I, M, J, vtype=GRB.BINARY, name="y_after")
         # w_i1_m1_i2_m2 = 1 if maintenance timing of machine m1 in stage i1 precedes maintenance timing of machine m2 in stage i2 or 0, otherwise, (i1, m1) != (i2, m2)
-        w = {}
-        for i1 in I:
-            M_i1 = M[i1 - 1]
+        self.w = {}
+        for i1 in self.I:
+            M_i1 = self.M[i1 - 1]
             for m1 in M_i1:
-                for i2 in I:
-                    M_i2 = M[i2 - 1]
+                for i2 in self.I:
+                    M_i2 = self.M[i2 - 1]
                     for m2 in M_i2:
                         if (i1, m1) != (i2, m2):
-                            w[i1, m1, i2, m2] = self.gp_model.addVar(vtype=GRB.BINARY, name=f"w_{i1}_{m1}_{i2}_{m2}")
-        # v_im: 1 if machine m in stage i is maintained or 0, otherwise
-        # z_im^R: Completion time of maintenance on machine m in stage i
-        # z_ij: Completion time of job j in stage i
-        # p_imj: Effective production time of job j on machine m in stage i
-        # t_ij: Tardiness of job j in stage i
+                            self.w[i1, m1, i2, m2] = self.gp_model.addVar(vtype=GRB.BINARY, name=f"w_{i1}_{m1}_{i2}_{m2}")
+
+    def add_constraints(self) -> None:
+        # define constraints
+        
+        # constraint1
+        for i in self.I:
+            M_i = self.M[i - 1]
+            for m in M_i:
+                for j in self.J:
+                    self.gp_model.addConstr(
+                        self.z_ij[i, j] - (self.parameters.Unfinished_Production_Time[i, m] + self.p_imj[i, m, j]) >= -1 * self.parameters.Very_Large_Positive_Number * (1 - self.r_imj[i, m, j])
+                    )
+        # constraint2
+        for i in self.I[:-1]:
+            M_i = self.M[i - 1]
+            for m in M_i:
+                for j in self.J:
+                    self.gp_model.addConstr(
+                        self.z_ij[i + 1, j] - (self.z_ij[i, j] + self.p_imj[i + 1, m, j]) >= -1 * self.parameters.Very_Large_Positive_Number * (1 - self.r_imj[i + 1, m, j])
+                    )
+        # constraint3
+        for i in self.I:
+            for j1 in self.J:
+                for j2 in self.J:
+                    if j1 != j2:
+                        self.gp_model.addConstr(
+                            self.z_ij[i, j1] + self.p_imj[i, m, j2] - self.z_ij[i, j2] <= self.parameters.Very_Large_Positive_Number * (1 - self.x[i, m, j1, j2])
+                        )
+        # constraint4
+        for i in self.I:
+            for m in self.M[i - 1]:
+                self.z_imR[i, m] >= (
+                    self.parameters.Unfinished_Production_Time[i, m] + 
+                    self.parameters.Maintenance_Length[i, m] +
+                    self.parameters.Very_Large_Positive_Number * (quicksum(self.y_imj_before[i, m, j] for j in self.J) - self.parameters.Number_of_Jobs)
+                )
+    def set_objective(self) -> None:
+        # define objective function
+        self.gp_model.setObjective((quicksum(
+            max(self.z_ij[self.parameters.Number_of_Stages] - self.parameters.Due_Time[j], 0) * self.Data.WEIGHT[j] 
+            for j in self.Data.N_JOB
+        )), GRB.MINIMIZE)
