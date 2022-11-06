21646e23685d1a2ffb60407778b16e1dd7aa642e
沛妤 陳 <peifish1124@5xruby.com>
2022-11-06 16:32:49 +0800

    meta_ga.py & ga_selection.py

---
+++ extended/Models/heuristic/meta_ga.py
@@ -0,0 +1,18 @@
+from Models import Parameters, SolutionModel
+from utils.schedule_objective_value_calculation import calculate_objective_value, transform_parameters_to_instance, print_instance
+
+class MetaGAModel(SolutionModel):
+    def __init__(self, parameters: Parameters):
+        super().__init__(parameters)
+
+    def run_and_solve(self):
+        print("Running and solving using GAModel")
+
+    def record_result(self):
+        print("Recording result using GAModel")
+        example_schedule = [[1.2, 1.3, 2.5, 0, 2.3], [1.2, 1.5, 1.3, 1.1, 0]]
+        instance = transform_parameters_to_instance(self.parameters)
+        print("Instance after transformation:")
+        print_instance(instance)
+        schedule_obj = calculate_objective_value(example_schedule, instance)
+        print("Schedule objective value: ", schedule_obj)
