fcd29d66cc02eed1903510f505377773ad39095b
沛妤 陳 <peifish1124@5xruby.com>
2022-11-20 09:55:56 +0800

    fix mutation

--- extended/utils/ga_mutation.py
+++ extended/utils/ga_mutation.py
@@ -1,11 +1,20 @@
 import random
+import math
 
 # ex: job_possiblility-> 0.0007
-def ga_mutation(schedule, job_possibility, machine_possibility, job_num, max_machine_num) -> list:
+# ex: iteration -> 1
+# ex: job_float_order -> [0.2, 0.4, 0.6, 0.8]
+def ga_mutation(schedule, job_possibility, machine_possibility, job_num, max_machine_num, iteration, job_float_order) -> list:
   # job
   if random.random() < job_possibility:
     chosen_station_num = random.randint(0,len(schedule)-1)
-    schedule[chosen_station_num].reverse()
+    job_schedule = schedule[chosen_station_num][:job_num]
+    job_schedule.reverse()
+    machine_schedule = schedule[chosen_station_num][-(max_machine_num):]
+    schedule[chosen_station_num] = job_schedule + machine_schedule
+    for i in range(len(schedule[chosen_station_num])):
+      if i < job_num:
+        schedule[chosen_station_num][i] = math.floor(schedule[chosen_station_num][i]) + job_float_order[i]
 
   # machine
   if random.random() < machine_possibility:
@@ -16,12 +25,14 @@ def ga_mutation(schedule, job_possibility, machine_possibility, job_num, max_mac
     for machine_ind in range(max_machine_num):
       job_on_machine_dict[machine_ind] = []
     for job_ind in range(job_num):
-      int_part = int(schedule[chosen_station_num][job_ind])
+      int_part = math.floor(schedule[chosen_station_num][job_ind])
       job_on_machine_dict[int_part-1].append(job_ind)
     for machine_ind in range(max_machine_num):
       if len(job_on_machine_dict[machine_ind]) != 0:
-        temp_value = schedule[chosen_station_num][job_num+machine_ind]
         chosen_job_index = random.choice(job_on_machine_dict[machine_ind])
-        schedule[chosen_station_num][job_num+machine_ind] = schedule[chosen_station_num][chosen_job_index]
-        schedule[chosen_station_num][chosen_job_index] = temp_value
+        offset = (0.1 ** (iteration+2))/(job_num+1)
+        if schedule[chosen_station_num][job_num+machine_ind] > schedule[chosen_station_num][chosen_job_index]:
+          schedule[chosen_station_num][job_num+machine_ind] = schedule[chosen_station_num][chosen_job_index] - offset
+        else:
+          schedule[chosen_station_num][job_num+machine_ind] = schedule[chosen_station_num][chosen_job_index] + offset
   return schedule
