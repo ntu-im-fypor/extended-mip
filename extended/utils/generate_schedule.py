79579f5681ddfbc1fe11adfe4ad81405796d3f67
Sherry Yeh <b08705053@ntu.edu.tw>
2022-11-25 01:11:55 +0800

    update maintenance conflict elimination

--- extended/utils/generate_schedule.py
+++ extended/utils/generate_schedule.py
@@ -96,13 +96,26 @@ def compute_tardiness(current_end_time, instance):
     return obj
 
 
-# TODO: sort maintenance order according to stage and machine length
-def sort_maintenance(maint_order):
-    return maint_order
+
 
 
 # function for generating schedule and computing objective value
 def generate_schedule(shared_job_order, order_on_machines, instance) -> float:
+
+    def sort_maintenance(maint_order): # TODO: sort maintenance order according to stage and machine length
+        # print('maint_order', maint_order)
+        job_priority = [[0, 0] for i in range(len(maint_order))]
+        for i in range(len(maint_order)):
+            job_priority[i][0] = maint_order[i]
+            job_after_maintenance = order_on_machines[maint_order[i]][current_machine_index[maint_order[i]]+1]
+            job_priority[i][1] = shared_job_order.index(job_after_maintenance) # sorting criteria: next job in shared job order
+        job_priority = sorted(job_priority, key=itemgetter(1))  # sort by first value of tuple
+        
+        if job_priority != []: # get first element using zip, then change data type to list
+            maint_order = list(list(zip(*job_priority))[0])
+        
+        return maint_order
+    
     current_end_time = [0] * instance.JOBS_NUM
     total_machines_num = sum(instance.MACHINES_NUM)
     current_machine_time = [0] * total_machines_num
@@ -117,7 +130,7 @@ def generate_schedule(shared_job_order, order_on_machines, instance) -> float:
         maint_order = [] # store the maintenance order
         for j in range(total_machines_num): # mark all machines that need maintenance
             if (current_machine_index[j] != len(order_on_machines[j])):
-                if (order_on_machines[j][current_machine_index[j]] == 'M'):
+                if (order_on_machines[j][current_machine_index[j]] == 'M' and current_machine_index[j] != len(order_on_machines[j])-1):
                     maint_order.append(j) # append the machine that needs maintenance
         sorted_maintenance_order = sort_maintenance(maint_order) # sort the maintenance order
         # print(sorted_maintenance_order)
