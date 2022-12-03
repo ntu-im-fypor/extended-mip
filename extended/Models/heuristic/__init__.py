add75ebb4452eb2639ed9091b546007f9a35cd12
Jenny Wu <wcy881218@gmail.com>
2022-12-03 11:13:15 +0800

    update GA

--- extended/Models/heuristic/__init__.py
+++ extended/Models/heuristic/__init__.py
@@ -1,3 +1,3 @@
 from .meta_pso import MetaPSOModel
 from .meta_ga import MetaGAModel
-from .ours_greedy import GreedyModel
+# from .ours_greedy import GreedyModel
--- extended/main.py
+++ extended/main.py
@@ -5,19 +5,23 @@ from tokenize import String
 from unittest import result
 from Models import Parameters
 from Models.Gurobi import CompleteMIPModel, CompleteMIPModel_original
-from Models.heuristic import MetaPSOModel, MetaGAModel, GreedyModel
+# from Models.heuristic import MetaPSOModel, MetaGAModel, GreedyModel
+from Models.heuristic import MetaGAModel
+import time
+
+
 
 def test_relaxation_result():
     result = []
-    for i in range(1, 51):
-        file_path = "/Users/ckocp3/Downloads/base/base_" + str(i) + ".txt"
-        # file_path = "/Users/ckocp3/Downloads/base_1105/base_" + str(i) + ".txt"
-        result_path = "/Users/ckocp3/Downloads/base_result_1107/base_result_" + str(i) + ".txt"
-        parameters = Parameters()
-        parameters.read_parameters(file_path)
-        # build and solve the model
-        model = CompleteMIPModel(parameters)
-        result.append(model.run_and_solve(result_path))
+    # for i in range(1, 51):
+    #     file_path = "/Users/ckocp3/Downloads/base/base_" + str(i) + ".txt"
+    #     # file_path = "/Users/ckocp3/Downloads/base_1105/base_" + str(i) + ".txt"
+    #     result_path = "/Users/ckocp3/Downloads/base_result_1107/base_result_" + str(i) + ".txt"
+    #     parameters = Parameters()
+    #     parameters.read_parameters(file_path)
+    #     # build and solve the model
+    #     model = CompleteMIPModel(parameters)
+    #     result.append(model.run_and_solve(result_path))
 
     # workbook = xlsxwriter.Workbook('relaxation_result.xlsx')
     # worksheet = workbook.add_worksheet()
@@ -45,47 +49,69 @@ def test_heuristic_model():
     # parameters.read_parameters(file_path)
     # use input to choose which model to use
     model_type = input("Please choose which model to use: 1. MetaPSOModel, 2. MetaGAModel, 3. GreedyModel, 4. CompleteMIPModel")
-    df = pd.DataFrame(index=range(1, 51), columns=[ # base_1125/base_1130: 51, 學姊's benchmark: 31
-        "initial objective value", "initial shared job order", "initial schedule",
-        "process objective value", "process shared job order", "process schedule",
-        "final objective value", "final shared job order", "final schedule",
-        "time"])
-    for i in range(50): # base_1125/base_1130: 50, 學姊's benchmark: 30 
-        print("base_" + str(i+1))
-        # test with base_1125
-        file_path = "tests/base_1125/base_" + str(i+1) + ".txt"
-        # test with base_1130
-        # file_path = "tests/base_1130/base_" + str(i+1) + ".txt"
-        # test with 學姊's benchmark
-        # file_path = "tests/benchmark/benchmark_" + str(i+1) + ".txt"
-        parameters = Parameters()
-        parameters.read_parameters(file_path)  
-        start_time = time.time()
-        heuristic_model = None
-        if model_type == "1":
-            heuristic_model = MetaPSOModel(parameters)
-        elif model_type == "2":
-            heuristic_model = MetaGAModel(parameters)
-        elif model_type == "3":
-            heuristic_model = GreedyModel(parameters, file_path="greedy-results/test.json")
-        elif model_type == "4":
-            heuristic_model = CompleteMIPModel(parameters)
-        else:
-            print("Invalid model type")
-            return
+    heuristic_model = None
 
-        heuristic_model.run_and_solve()
-        df = heuristic_model.record_result(df, i)
-        run_time = time.time() - start_time
-        df.iloc[i]["time"] = run_time
-        print("Run time: ", run_time)
-        print("=====")
-    # test with base_1125
-    df.to_csv('greedy-results/base_1125.csv')
-    # test with base_1130
-    # df.to_csv('greedy-results/base_1130.csv')
-    # test with 學姊's benchmark
-    # df.to_csv('greedy-results/benchmark.csv')
+    # iterate all instance
+    population_number = [30, 50]
+    machine_mutation_rate =  [0.05, 0.1]
+    job_mutation_rate = [0.05, 0.1]
+
+    for p_num in population_number:
+        for m_rate in machine_mutation_rate:
+            for j_rate in job_mutation_rate:
+                result = {
+                "final objective value": [],
+                "first objective value": [],
+                "final shared job order": [],
+                "final schedule": [],
+                "time": []
+                }
+
+                for i in range(50):
+                    file_path = "tests/base_1130/base_" + str(i+1) + ".txt"
+                    parameters = Parameters()
+                    parameters.read_parameters(file_path)
+
+                    # create model
+                    if model_type == "1":
+                        heuristic_model = MetaPSOModel(parameters)
+                    elif model_type == "2":
+                        heuristic_model = MetaGAModel(parameters)
+                    elif model_type == "3":
+                        heuristic_model = GreedyModel(parameters, file_path="greedy-results/test.json")
+                    elif model_type == "4":
+                        heuristic_model = CompleteMIPModel(parameters)
+                    else:
+                        print("Invalid model type")
+                        return
+
+
+
+                    # Parameter Tuning
+                    ''''
+                    1. population number: [30, 40, 50]
+                    2. machine mutation rate: [0.02, 0.04, 0.06, 0.08, 0.1]
+                    3. job mutation rate: [0.02, 0.04, 0.06, 0.08, 0.1]
+                    '''
+
+                    init_time = time.time()
+                    heuristic_model.run_and_solve(
+                        job_mutation_rate = j_rate,
+                        machine_mutation_rate = m_rate,
+                        population_num = p_num)
+                    obj, share_job_order, schedule, init_obj = heuristic_model.record_result()
+                    finish_time = time.time()
+                    result['final objective value'].append(obj)
+                    result['final shared job order'].append(share_job_order)
+                    result['final schedule'].append(schedule)
+                    result['time'].append(finish_time-init_time)
+                    result['first objective value'].append(init_obj)
+                    print("duration = ", finish_time-init_time)
+
+                output = pd.DataFrame(result)
+                output_filename = 'GA-results/GA_base_1202_' + 'p_num_' + str(p_num) + '_m_rate_' + str(m_rate) + '_j_rate_' + str(j_rate) + '.csv'
+                output.to_csv(output_filename, index=True)
+                print('====== finish ', output_filename, ' =========')
 
 if __name__ == '__main__':
     # test_relaxation_result()
