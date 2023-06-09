405e70ff6d888bd85b31095535fa15f9a1700178
ckocp3 <2a2s9d0f5g@gmail.com>
2023-06-09 19:26:06 +0800

    delete unused files

--- extended/Models/__init__.py
+++ extended/Models/__init__.py
@@ -1,4 +1,3 @@
 from .Parameters import Parameters
 from .Gurobi.complete_mip_gurobi import CompleteMIPModel
-from .Gurobi.gurobi_original_problem import CompleteMIPModel_original
 from .solution_model import SolutionModel
--- extended/Models/heuristic/__init__.py
+++ extended/Models/heuristic/__init__.py
@@ -1,3 +1,1 @@
-from .meta_pso import MetaPSOModel
-from .meta_ga import MetaGAModel
 from .ours_greedy import GreedyModel
